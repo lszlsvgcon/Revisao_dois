7737b3bac523916476e1727a2f4bc48d226d1051
lszlsvgcon <lsfmaillsvl@gmail.com>
2024-04-09 00:17:20 -0300

    Resolve questão 1

---
+++ entrada_e_saida.py
@@ -0,0 +1,8 @@
+#1 - Crie uma função que pede o nome do usuário e o imprime com uma saudação. Ao final desta tarefa faça um commit “Resolve questão 1”no repositório remoto.
+#Exemplo de resultado: "Olá, [nome do usuário]! Seja bem-vindo(a)!"
+
+
+def saudacao_usuario():
+    nome = input("Digite o seu nome: ")
+    print(f"Olá, {nome}! Seja bem-vindo(a)!")
+saudacao_usuario()
